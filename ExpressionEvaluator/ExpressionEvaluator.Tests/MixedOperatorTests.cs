5f30e3f97b5afb8cc7c9a59d1b8315d3052d77b1
hyungkyu <hyungkyu@deepeyes.co.uk>
2021-02-27 21:17:54 +0000

    oder of operations with brackets updated

--- ExpressionEvaluator/ExpressionEvaluator.Tests/MixedOperatorTests.cs
+++ ExpressionEvaluator/ExpressionEvaluator.Tests/MixedOperatorTests.cs
@@ -18,26 +18,43 @@ namespace ExpressionEvaluator.Tests
         public void Should_evluate_mixed_operator_expression()
         {
             // arrange
-            var input = "1+2+1*2+1";
+            var input = "1+3+1*2+1";
             
             // act
             var result = _evaluator.Evaluate(input);
 
             // assert
-            Assert.That(result, Is.EqualTo(6));
+            Assert.That(result, Is.EqualTo(7));
         }
         
         [Test]
         public void Should_evluate_mixed_operator_expression_with_spaces()
         {
             // arrange
-            var input = "1 +2 +1* 2+1";
+            var input = "1 +3 +1* 2+1";
             
             // act
             var result = _evaluator.Evaluate(input);
 
             // assert
-            Assert.That(result, Is.EqualTo(6));
+            Assert.That(result, Is.EqualTo(7));
+        }
+        
+        [Test]
+        public void Should_evluate_mixed_operator_expression_with_brackets()
+        {
+            // arrange
+            var input = "2 *((3 +4)+ 2)+ (2+3)*4";
+            
+            // act
+            var result = _evaluator.Evaluate(input);
+            var example3 = _evaluator.Evaluate("(1+2+1)");
+            var example4 = _evaluator.Evaluate("((1+2+1)*(1+1))");
+
+            // assert
+            Assert.That(result, Is.EqualTo(38));
+            Assert.That(example3, Is.EqualTo(4));
+            Assert.That(example4, Is.EqualTo(8));
         }
     }
 }
--- ExpressionEvaluator/ExpressionEvaluator/Domains/Evaluator.cs
+++ ExpressionEvaluator/ExpressionEvaluator/Domains/Evaluator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ExpressionEvaluator.Domains
 {
@@ -9,14 +10,49 @@ namespace ExpressionEvaluator.Domains
         public int Evaluate(string input)
         {
             if (!input.Contains("+") &&
-                !input.Contains("*"))
+                !input.Contains("*") &&
+                !input.Contains("(") &&
+                !input.Contains(")"))
             {
                 var element = new Elem(input);
                 return element.Number;
             }
 
-            // var delimiters = new string[] {"+", "*", "(", ")"};
+            var updatedExpression = "";
+            
+            updatedExpression = HighOrderBracketOperation(input);
 
+            return MixedOperation(updatedExpression);
+        }
+
+        private string HighOrderBracketOperation(string input)
+        {
+            while (true)
+            {
+                var pattern = @"\([\d\s\*\+]+\)";
+                var regEx = new Regex(pattern);
+                var bracketExpressions = regEx.Matches(input);
+
+                if (bracketExpressions.Count <= 0)
+                {
+                    break;
+                }
+
+                var delimiters = new char[] {'(', ')'};
+
+
+                foreach (Match expression in bracketExpressions)
+                {
+                    var braketResult = MixedOperation(expression.Value.Trim(delimiters)).ToString();
+                    input = input.Replace(expression.Value, braketResult);
+                }
+            }
+
+            return input;
+        }
+
+        private int MixedOperation(string input)
+        {
             var addExpressions = input.Split("+");
             var elements = new List<string>();
             var result = 0;
@@ -27,7 +63,7 @@ namespace ExpressionEvaluator.Domains
                     elements.Add(expression);
                     continue;
                 }
-                
+
                 var multiplyExpressions = expression.Split("*");
                 result += Multiply(multiplyExpressions.Select(x => new Elem(x)));
             }
